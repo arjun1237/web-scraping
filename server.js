3a681a47976527d4da784cc8c6bcb407171a927b
aravind <aravind.sakthi@outlook.com>
2021-04-14 12:27:30 +0530

    new

--- server.js
+++ server.js
@@ -4,12 +4,8 @@ const scraperRouter = require('./resources/router/scraper.router')
 
 app.use(express.json())
 
-const start = async () => {
-    app.listen(5050, () => {
-        console.log("listening to port 5050")
-    })
-}
-
-start()
-
 app.use('/scraper', scraperRouter)
+
+app.listen(5050, () => {
+  console.log("listening to port 5050");
+});
