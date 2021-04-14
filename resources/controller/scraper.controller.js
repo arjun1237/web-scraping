a7021933221b8a67d68e7e045060adf99d30b722
Arjun K Prasad <arjun1237.kp@gmail.com>
2021-04-14 13:03:50 +0530

    error handling completed

--- resources/controller/scraper.controller.js
+++ resources/controller/scraper.controller.js
@@ -16,39 +16,44 @@ const scrapeData = async (req, res) => {
     // scraper logic
     let { url } = req.body
     request(url, (err, response, html) => {
-        if(!err && response.statusCode === 200){
-            const $ = cheerio.load(html)
-
-            const title = $("meta[property='og:title']")[0].attribs.content
-            const image = $("meta[property='og:image']")[0].attribs.content
-            const description = $("meta[property='og:description']")[0].attribs.content
-            const site_name = $("meta[property='og:site_name']")[0].attribs.content
-            const type = $("meta[property='og:type']")[0].attribs.content
-            const url_name = $("meta[property='og:url']")[0].attribs.content
-            const icons = []
-
-            $("link").each(function (index, element) {
-                let rel = $(element).attr('rel')
-                if(rel.includes('icon')){
-                    icons.push($(element).attr('href'))
+        try{
+            if(!err && response.statusCode === 200){
+                const $ = cheerio.load(html)
+    
+                const title = $("meta[property='og:title']")[0] ? $("meta[property='og:title']")[0].attribs.content : undefined
+                const image = $("meta[property='og:image']")[0] ? $("meta[property='og:image']")[0].attribs.content : undefined
+                const description = $("meta[property='og:description']")[0] ? $("meta[property='og:description']")[0].attribs.content : undefined
+                const site_name = $("meta[property='og:site_name']")[0] ? $("meta[property='og:site_name']")[0].attribs.content : undefined
+                const type = $("meta[property='og:type']")[0] ? $("meta[property='og:type']")[0].attribs.content : undefined
+                const url_name = $("meta[property='og:url']")[0] ? $("meta[property='og:url']")[0].attribs.content : undefined
+                const icons = []
+    
+                $("link").each(function (index, element) {
+                    let rel = $(element).attr('rel')
+                    if(rel.includes('icon')){
+                        icons.push($(element).attr('href'))
+                    }
+                });
+                let icon = icons[0]
+    
+                const data = {
+                    site_name,
+                    icon,
+                    title, 
+                    description,
+                    image,
+                    type,
+                    url: url_name
                 }
-            });
-            let icon = icons[0]
-
-            const data = {
-                site_name,
-                icon,
-                title, 
-                description,
-                image,
-                type,
-                url: url_name
+    
+                res.json({error: false, data})
             }
-
-            res.json({error: false, data})
+            else{
+                throw new Error(err.message)
+            }
         }
-        else{
-            res.status(400).json({error: true, message: err.message})
+        catch(err){
+            return res.status(400).json({error: true, message: err.message})
         }
     })
 }
