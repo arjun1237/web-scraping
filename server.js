12c6390935482b8c30d7471bd645a772f1a372f8
aravind <aravind.sakthi@outlook.com>
2021-04-14 12:31:43 +0530

    new

--- server.js
+++ server.js
@@ -1,11 +1,11 @@
 const express = require('express')
 const app = express()
 const scraperRouter = require('./resources/router/scraper.router')
-
+const PORT = process.env.PORT || 5050
 app.use(express.json())
 
 app.use('/scraper', scraperRouter)
 
-app.listen(5050, () => {
-  console.log("listening to port 5050");
+app.listen(PORT, () => {
+  console.log(`listening to port ${PORT}`);
 });
